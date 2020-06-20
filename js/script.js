564709ca9f7f4a07652279c792e59a86a19fbbe0
DNyakonyu <65835865+DNyakonyu@users.noreply.github.com>
2020-06-20 09:57:54 +0300

    Updated feedback message.

--- js/script.js
+++ js/script.js
@@ -3,104 +3,79 @@ $(document).ready(function(){
 
 });
 
-(document).ready(function(){
-  $("#development-image").click(function(){
-    $("#development-image").slideDown('1500').hide('1000');
-    $("#development").show('1500');
-  });
-  $("#development").click(function(){
-    $("#development").slideUp('1500');
-    $("#development-image").slideDown('1500');
-  });
-});
 
-$(document).ready(function(){
-  $("#design-image").click(function(){
-    $("#design-image").slideDown('1500').hide('1000');
-    $("#design").show('1500');
-  });
-  $("#design").click(function(){
-    $("#design").slideUp('1500');
-    $("#design-image").slideDown('1500');
-  });
-});
+$(function () {
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
 
-$(document).ready(function(){
-  $("#product-image").click(function(){
-    $("#product-image").slideDown('1500').hide('1000');
-    $("#product").show('1500');
-  });
-  $("#product").click(function(){
-    $("#product").slideUp('1500');
-    $("#product-image").slideDown('1500');
-  });
-});
+    $(desp).hide();
+    $(devp).hide();
+    $(prop).hide();
 
-$(document).ready(function(){
-  $("#work1").mouseover(function(){
-    $("#overlay").show();
-  }).mouseout(function(){
-    $("#overlay").hide();
-  });
-});
-$(document).ready(function(){
-  $("#work2").mouseover(function(){
-    $("#overlay2").show();
-  }).mouseout(function(){
-    $("#overlay2").hide();
-  });
-});
-$(document).ready(function(){
-  $("#work3").mouseover(function(){
-    $("#overlay3").show();
-  }).mouseout(function(){
-    $("#overlay3").hide();
-  });
-});
-$(document).ready(function(){
-  $("#work4").mouseover(function(){
-    $("#overlay4").show();
-  }).mouseout(function(){
-    $("#overlay4").hide();
-  });
-});
+    //Show paragraph content
+    $('.des').on('click', function () {
+        $('.des').slideUp('slow');
+        $(desp).show(500);
+        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
 
-$(document).ready(function(){
-  $("#work5").mouseover(function(){
-    $("#overlay5").show();
-  }).mouseout(function(){
-    $("#overlay5").hide();
-  });
-  $("#work6").mouseover(function(){
-    $("#overlay6").show();
-  }).mouseout(function(){
-    $("#overlay6").hide();
-  });
-  $("#work7").mouseover(function(){
-    $("#overlay7").show();
-  }).mouseout(function(){
-    $("#overlay7").hide();
-  });
-  $("#work8").mouseover(function(){
-    $("#overlay8").show();
-  }).mouseout(function(){
-    $("#overlay8").hide();
-  });
-});
+    });
+    $('.dev').on('click', function () {
+        $('.dev').slideUp('slow');
+        $(devp).show(500);
+        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+    $('.pro').on('click', function () {
+        $('.pro').slideUp('slow');
+        $(prop).show(500);
+        $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
 
-$(document).ready(function(){
-  $("form#form34A").submit(function(event){
-    // event.preventDefault();
-    var name = $("input#MERGE1").val();
-    var email = $("input#MERGE0").val();
-    var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-      alert (name + ", we have received your message. Thank you forcontacting us.");
-    }
-    else {
-      alert("Please enter your name and email!");
-    }
+    //Hide paragraph content
+    $(desp, destext).on('click', function () {
+        $('.des').slideDown();
+        $(desp).hide(500);
+        $(destext).animate({fontWeight:'normal',fontSize:'16px'})
+    });
+    $(devp, devtext).on('click', function () {
+        $('.dev').slideDown();
+        $(devp).hide(500);
+        $(devtext).animate({fontWeight:'normal',fontSize:'16px'})
+    });
+    $(prop, protext).on('click', function () {
+        $('.pro').slideDown();
+        $(prop).hide(500);
+        $(protext).animate({fontWeight:'normal',fontSize:'16px'})
+    })
 
-  });
+    //Overlay images
+
+    $('.portimg').hover(function(){
+        $('.porttext',this).slideToggle('slow');
+     }, function(){
+        $('.porttext',this).slideToggle('slow');
+     });
+
+
+    //Form Validation
+        $("form#form").on('submit',function(event){
+            event.preventDefault();
+            let name = $("input#name").val();
+            let email = $("input#email").val();
+            let message = $("textarea#message").val();
+
+            if ($("input#name").val() && $("input#email").val()){
+                alert ("Hello " + name + ", Thanks for reaching out, we'll get back to you soon");
+            }
+            else {
+                alert("Please provide your correct name and email!");
+            }
+
+        });
+
+
 
 });
